1b926a03edd70f21db359e9fcd5277a2bbaac3b1
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-02-07 13:50:26 -0500

    add error handling controller

--- practice_assignments/SpringBootDemo/src/main/java/org/perscholas/controller/AdminController.java
+++ practice_assignments/SpringBootDemo/src/main/java/org/perscholas/controller/AdminController.java
@@ -2,6 +2,7 @@ package org.perscholas.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,18 +10,18 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
-
 @Controller
-@RequestMapping("/login")
-public class LoginController {
+@RequestMapping("/admin")
+@PreAuthorize("hasAuthority('ADMIN')")
+public class AdminController {
 
-    public static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
+    public static final Logger LOG = LoggerFactory.getLogger(AdminController.class);
 
-    @RequestMapping(value = {  "/login" }, method = RequestMethod.GET)
-    public ModelAndView login() throws Exception {
+    @RequestMapping(value = {  "/home" }, method = RequestMethod.GET)
+    public ModelAndView adminHome() throws Exception {
         ModelAndView response = new ModelAndView();
-        response.setViewName("login/login");
+        response.setViewName("admin/home");
         return response;
     }
-
 }
+
---
+++ practice_assignments/SpringBootDemo/src/main/webapp/WEB-INF/jsp/admin/home.jsp
@@ -0,0 +1,6 @@
+<jsp:include page="../include/header.jsp" />
+
+<h1> This is an admin page</h1>
+
+
+<jsp:include page="../include/footer.jsp" />
