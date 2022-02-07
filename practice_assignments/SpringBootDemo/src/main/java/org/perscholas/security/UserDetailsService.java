aa8a69c8eee04619f0bd8544371c6fb7b6e763bc
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-02-07 10:49:06 -0500

    update UserDetailsService and UserDetailsServiceImpl

--- practice_assignments/SpringBootDemo/src/main/java/org/perscholas/security/UserDetailsService.java
+++ practice_assignments/SpringBootDemo/src/main/java/org/perscholas/security/UserDetailsService.java
@@ -1,6 +1,14 @@
 package org.perscholas.security;
 
+import org.perscholas.database.entity.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
 public interface UserDetailsService {
 
+    public UserDetails loadUserByUsername(String username);
 
 }
--- practice_assignments/SpringBootDemo/src/main/java/org/perscholas/security/UserDetailsServiceImpl.java
+++ practice_assignments/SpringBootDemo/src/main/java/org/perscholas/security/UserDetailsServiceImpl.java
@@ -5,15 +5,17 @@ import org.perscholas.database.entity.User;
 import org.perscholas.database.entity.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @Component
-public class UserDetailsServiceImpl {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserDAO userDao;
@@ -39,6 +41,18 @@ public class UserDetailsServiceImpl {
         //setup user roles
         Collection<? extends GrantedAuthority> springRoles = buildGrantAuthorities(userRoles);
 
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), accountIsEnabled, accountNotExpired, credentialsNonExpired)
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), accountIsEnabled,
+                                                                        accountNotExpired, credentialsNonExpired, accountNonLocked, springRoles);
+        }
+
+        private Collection<? extends GrantedAuthority> buildGrantAuthorities(List<UserRole> userRoles){
+
+            List<GrantedAuthority> authorities = new ArrayList<>();
+
+            for (UserRole role : userRoles){
+                authorities.add(new SimpleGrantedAuthority(role.getUserRole().toString()));
+            }
+
+            return authorities;
         }
 }
