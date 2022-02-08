48788699dbb806f344cabd1df462faa926ef7ea8
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-02-08 11:54:37 -0500

    new functionality: find username with spring security then use userDAO to find user in database, then add user object to response to make user data available to use on the jsp paghe

--- practice_assignments/SpringBootDemo/src/main/java/org/perscholas/controller/UserController.java
+++ practice_assignments/SpringBootDemo/src/main/java/org/perscholas/controller/UserController.java
@@ -1,6 +1,11 @@
 package org.perscholas.controller;
 
+import org.perscholas.database.dao.UserDAO;
+import org.perscholas.database.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,10 +15,23 @@ import org.springframework.web.servlet.ModelAndView;
 @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
 public class UserController {
 
+    @Autowired
+    private UserDAO userDao;
+
     @RequestMapping(value = {  "/user/profile" }, method = RequestMethod.GET)
     public ModelAndView userProfile() throws Exception {
         ModelAndView response = new ModelAndView();
         response.setViewName("user/profile");
+
+        //this gets username from spring security
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+
+        //uses currentPrincipalName to find user info from the database
+        User user = userDao.findByEmail(currentPrincipalName);
+        response.addObject("userProfile", user);
+
+
         return response;
     }
 }
--- practice_assignments/SpringBootDemo/src/main/webapp/WEB-INF/jsp/user/profile.jsp
+++ practice_assignments/SpringBootDemo/src/main/webapp/WEB-INF/jsp/user/profile.jsp
@@ -3,7 +3,14 @@
 
 <h1> This is the user profile page</h1>
 <%--this allows you to display user info from the database--%>
+<%--more info can be found here https://www.baeldung.com/spring-security-taglibs--%>
 <h2>The logged in user is: <sec:authentication property="principal.username"></sec:authentication></h2>
+<ul>
+    <li>ID: ${userProfile.id}</li>
+    <li>Email: ${userProfile.email}</li>
+    <li>First Name: ${userProfile.firstName}</li>
+    <li>Last Name: ${userProfile.lastName}</li>
+</ul>
 
 
 <jsp:include page="../include/footer.jsp" />
