227a162e263c24107356c1f3e42444e6fda25c55
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-02-09 13:58:33 -0500

    add Create functionality with POST method

--- practice_assignments/SpringBootDemo/src/main/java/org/perscholas/controller/RestController.java
+++ practice_assignments/SpringBootDemo/src/main/java/org/perscholas/controller/RestController.java
@@ -73,7 +73,7 @@ public class RestController {
 //        return jo.toString();
     }
 
-    @RequestMapping(value="/{id}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value="/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public User add(@Valid RegisterFormBean form, BindingResult errors) {
         User user = new User();
 
